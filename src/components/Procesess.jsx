a93a9ee6c0be023a10dfdc8d1dc0204d421dbc63
Pratham <prathamsinghal2003@gmail.com>
2023-12-31 15:39:49 +0530

    feat: add loader

--- src/components/Procesess.jsx
+++ src/components/Procesess.jsx
@@ -6,28 +6,35 @@ const Processes = (props) => {
   let process = [];
   let burst_time = [];
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState({});
   for (let i = 0; i < props.inputProcess; i++) {
     process.push(i);
     burst_time.push(i);
   }
   const handleSubmit = async () => {
+    setLoading(true)
     await axios
       .post("https://scheduling-backend.onrender.com/process/calculation-arrival-same", {
         processes: process,
         burstTime: burst_time,
       })
       .then((response) => {
+        setLoading(false);
         setData(response.data);
         setShow(true);
       })
       .catch((error) => {
+        setLoading(false);
         console.log(error);
         setShow(false);
       });
   };
   return (
     <div className="flex flex-col ">
+      {
+        loading && <h2 className="text-center text-xl">Loading...</h2>
+      }
       <div className="flex text-xl min-w-fit mx-auto w-[24vw] ">
         <h2 className="my-2 mx-1 ">Process</h2>
         <h2 className="m-2">Burst Time (ms)</h2>
